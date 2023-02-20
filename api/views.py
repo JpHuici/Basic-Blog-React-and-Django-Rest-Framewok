6149919ac589539ef015b6ea274171a6123c7495
Juan Pablo Huici <jphuici1@gmail.com>
2023-02-20 15:14:12 -0300

    views added

--- api/views.py
+++ api/views.py
@@ -7,5 +7,37 @@ from .serializers import BlogSerializer
 
 @api_view(['GET'])
 def getBlogs(request):
-    pass
+    blog = Blog.objects.all()
+    serializer = BlogSerializer(blog, many=True)
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def getBlog(request):
+    data = request.data
+    blog = Blog.object.create(
+        title= data['title'],
+        body= data['body']
+    )
+    serializer = BlogSerializer(blog, many=False)
+    return Response(serializer.data)
+
+
+@api_view(['PUT'])
+def putBlog(request, pk):
+    data = request.data
+    blog = Blog.objects.get(id=pk)
+    serializer = BlogSerializer(instance=blog, data=data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def deleteBlog(request,pk):
+    blog = Blog.objects.get(id=pk)
+    blog.delete()
+    return Response('Blog Deleted')
+
+
 
