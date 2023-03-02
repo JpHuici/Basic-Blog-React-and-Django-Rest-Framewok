009f9cb1ef782bfee5d46d9f4e5271400cdc7e7b
Juan Pablo Huici <jphuici1@gmail.com>
2023-03-01 22:16:24 -0300

    api app comments added and readme created

--- api/models.py
+++ api/models.py
@@ -1,5 +1,5 @@
 from django.db import models
 
-class Blog(models.Model):
+class Blog(models.Model): # Blog inherits from the models.Model class.
     title = models.CharField(max_length=25)
     body = models.CharField(max_length=250)
--- api/views.py
+++ api/views.py
@@ -4,15 +4,18 @@ from rest_framework.decorators import api_view
 
 from .models import Blog
 from .serializers import BlogSerializer
+'''
+This code defines four functions that use the Django Rest Framework (DRF) library to create an API to perform basic CRUD operations (Create, Read, Update, Delete) in the Blog model.
 
-@api_view(['GET'])
+'''
+@api_view(['GET']) # This function is decorated with @api_view(['GET']), indicating that it responds to GET requests. It gets all the Blog records in the database, serializes them with the BlogSerializer serializer and returns an HTTP response with the serialized data in JSON format.
 def getBlog(request):
     blog = Blog.objects.all()
     serializer = BlogSerializer(blog, many=True)
     return Response(serializer.data)
 
 
-@api_view(['POST'])
+@api_view(['POST']) # This function is decorated with @api_view(['POST']), indicating that it responds to POST requests. It receives the request data in request.data, creates a new record in the database using the received data, serializes it with the BlogSerializer serializer and returns an HTTP response with the serialized data in JSON format.
 def postBlog(request):
     data = request.data
     blog = Blog.object.create(
@@ -23,7 +26,7 @@ def postBlog(request):
     return Response(serializer.data)
 
 
-@api_view(['PUT'])
+@api_view(['PUT']) # This function is decorated with @api_view(['PUT']), indicating that it responds to PUT requests. It receives the request data in request.data and the identifier of the record to update in pk. It looks up the corresponding record in the database, updates it with the received data, serializes it with the BlogSerializer serializer and returns an HTTP response with the serialized data in JSON format.
 def putBlog(request, pk):
     data = request.data
     blog = Blog.objects.get(id=pk)
@@ -33,7 +36,7 @@ def putBlog(request, pk):
         return Response(serializer.data)
 
 
-@api_view(['DELETE'])
+@api_view(['DELETE']) # This function is decorated with @api_view(['DELETE']), indicating that it responds to DELETE requests. It receives the identifier of the record to delete in pk, looks for the corresponding record in the database and deletes it. It returns an HTTP response indicating that the record was deleted.
 def deleteBlog(request,pk):
     blog = Blog.objects.get(id=pk)
     blog.delete()
