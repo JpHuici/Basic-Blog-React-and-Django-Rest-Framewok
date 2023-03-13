4393af5bdeaba0d65a0e462061720c93b293c0d6
Juan Pablo Huici <jphuici1@gmail.com>
2023-03-13 15:28:20 -0300

    apps added to settings and started with models in users

--- users/models.py
+++ users/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
 
-# Create your models here.
+
+class CustomAccountManage(BaseUserManager):
+    def create_user(self, ):
+        pass
