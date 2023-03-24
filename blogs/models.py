717e95e53403865c54c637db234553e2dac14ac7
Juan Pablo Huici <jphuici1@gmail.com>
2023-03-24 12:52:41 -0300

    feat: backend setting and urls, blogs models.
    Created serializers and urls for users

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+class Blog():
+    body = models.CharField(("Write something here"), max_length=50)
+    user = models.ForeignKey("app.Model", verbose_name=(""), on_delete=models.CASCADE)
+    date = models.DateTimeField( auto_now=False, auto_now_add=False)
+
+class Comment():
+    blog = models.ForeignKey("app.Model", verbose_name=(""), on_delete=models.CASCADE)
+    user = models.ForeignKey("app.Model", verbose_name=(""), on_delete=models.CASCADE)
+    text = models.CharField((""), max_length=50)
+    date = models.DateTimeField((""), auto_now=False, auto_now_add=False)
---
+++ users/serializers.py
@@ -0,0 +1,11 @@
+import serializers
+import Refreshtoken
+from . import models
+
+class UserSerializer(self,):
+    is_admin = ""
+    class Meta:
+        model = ""
+        fields = ""
+        def get_is_admin():
+            return obj.is_staff
