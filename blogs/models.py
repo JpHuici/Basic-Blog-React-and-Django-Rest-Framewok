bc53ba5ca844634bba1b23af20d040f326e13f9f
Juan Pablo Huici <jphuici1@gmail.com>
2023-03-27 15:10:43 -0300

    feat: change models from blogs
    Blog and Comment

--- blogs/models.py
+++ blogs/models.py
@@ -2,12 +2,12 @@ from django.db import models
 from users.models import User
 
 class Blog():
-    body = models.CharField(("Write something here"), max_length=50)
-    user = models.ForeignKey("app.Model", verbose_name=(""), on_delete=models.CASCADE)
-    date = models.DateTimeField( auto_now=False, auto_now_add=False)
+    body = models.CharField(("Write something here"), max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    date = models.DateTimeField(auto_now_add=True)
 
 class Comment():
-    blog = models.ForeignKey("app.Model", verbose_name=(""), on_delete=models.CASCADE)
-    user = models.ForeignKey("app.Model", verbose_name=(""), on_delete=models.CASCADE)
-    text = models.CharField((""), max_length=50)
-    date = models.DateTimeField((""), auto_now=False, auto_now_add=False)
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    text = models.CharField((""), max_length=150)
+    date = models.DateTimeField(auto_now_add=True)
