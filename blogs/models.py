d828aca48eaadca67e8ece5859b969dac63548ec
Juan Pablo Huici <jphuici1@gmail.com>
2023-03-27 17:17:02 -0300

    feat: change models, add api views
    Settings from backend , urls views and serializers from users, models from blogs

--- blogs/models.py
+++ blogs/models.py
@@ -1,13 +1,13 @@
 from django.db import models
 from users.models import User
 
-class Blog():
-    body = models.CharField(("Write something here"), max_length=100)
+class Blog(models.Model):
+    body = models.CharField(max_length=100)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     date = models.DateTimeField(auto_now_add=True)
 
-class Comment():
+class Comment(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    text = models.CharField((""), max_length=150)
+    text = models.CharField(max_length=150)
     date = models.DateTimeField(auto_now_add=True)
